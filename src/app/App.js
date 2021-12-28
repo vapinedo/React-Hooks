4daaeccb9f5e88b444243002c905b5329b8028c8
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-28 07:58:59 -0500

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.css";
+
+export const App = () => {
+    return (
+        <main className="container main-container">
+            <h3>Hooks</h3>
+            <hr />
+        </main>
+    )
+}
