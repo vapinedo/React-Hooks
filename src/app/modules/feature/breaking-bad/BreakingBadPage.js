0575596be2687b005cb1350013c9ae239461caad
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-28 15:41:16 -0500

    BreakingBadPage updated

--- src/app/modules/feature/breaking-bad/BreakingBadPage.js
+++ src/app/modules/feature/breaking-bad/BreakingBadPage.js
@@ -3,13 +3,26 @@ import { useFetch } from '@core/hooks/useFetch'
 
 export const BreakingBadPage = () => {
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const { isLoading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    const { author, quote } = !!data && data[0];
     
     return (
         <>
             <h3>Breaking Bad Quotes</h3>   
             <hr />
+
+            { 
+                isLoading && 
+                <div className="alert alert-info">loading...</div>
+            }  
+
+            {
+                !isLoading && 
+                <blockquote className="blockquote text-end">
+                    <p>{quote}</p>
+                    <footer className="blockquote-footer">{author}</footer>
+                </blockquote>
+            }
         </>
     )
 }
