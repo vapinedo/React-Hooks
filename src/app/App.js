2502df4d7deff003ce3b50181cd28b1460df90a0
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-29 08:42:14 -0500

    FocusPage added

--- src/app/App.js
+++ src/app/App.js
@@ -5,12 +5,13 @@ import "./App.css";
 // import { CounterWithCustomHook } from '@feature/counter/pages/CounterWithCustomHook';
 // import { SimpleFormPage } from '@feature/form/pages/SimpleFormPage';
 // import { FormWithCustomHookPage } from '@feature/form/pages/FormWithCustomHookPage';
-import { BreakingBadPage } from '@feature/breaking-bad/BreakingBadPage';
+// import { BreakingBadPage } from '@feature/breaking-bad/BreakingBadPage';
+import { FocusPage } from '@feature/focus/FocusPage';
 
 export const App = () => {
     return (
         <main className="container main-container">
-            <BreakingBadPage />
+            <FocusPage />
         </main>
     )
 }
---
+++ src/app/modules/feature/focus/FocusPage.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+
+export const FocusPage = () => {
+
+    const inputRef = useRef();
+
+    const onFocus = () => {
+        inputRef.current.select();
+    }
+
+    return (
+        <div>
+            <h3>Focus page</h3>
+            <hr />
+
+            <input ref={inputRef} type="text" className="form-control mt-4" placeholder="Tu nombre" />
+            <button onClick={onFocus} className="btn btn-primary mt-4">Focus</button>
+        </div>
+    )
+}
